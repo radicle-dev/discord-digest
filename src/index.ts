fa08c59f036da7a9f02418c4c6de2d64d6e68e5f
Georgios Jason Efstathiou <j@jason-e.dev>
2022-07-29 10:26:11 +0200

    chore: request verification and import env vars

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-import { Request, Response } from 'express';
+import { verifyKey } from "discord-interactions";
+import { Request, Response } from "@google-cloud/functions-framework/build/src/functions";
+
+function validate(req: Request, res: Response) {
+  const signature = req.get("X-Signature-Ed25519");
+  const timestamp = req.get("X-Signature-Timestamp");
+
+  if (!signature) {
+    return false;
+  }
+
+  if (!timestamp) {
+    return false;
+  }
+
+  if (!req.rawBody) {
+    return res.status(400);
+  }
+
+  if (!(signature && timestamp && req.rawBody)) return false;
+
+  return verifyKey(
+    req.rawBody,
+    signature,
+    timestamp,
+    "d31a0005ae0419b00cdae112902ea44ad5ad91c7887b487d02d5ad4806cbf598"
+  );
+}
 
 export function discordDigest(req: Request, res: Response) {
-  res.send('Hello World');
+  if (!validate(req, res)) return res.status(401);
+
+  return res.send({
+    type: 1,
+  });
 }
